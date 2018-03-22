25d529d7932582d0d98dc2518e5a42556aa0c53a
shyjues <shyju.subramanian@gmail.com>
2018-03-23 01:02:40 +1100

    again some code

--- src/main/java/com/shyju/carParking/App.java
+++ src/main/java/com/shyju/carParking/App.java
@@ -4,32 +4,31 @@ import java.util.Scanner;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App
-{
-	
+public class App {
+
 	private static ProcessMovement processMovement = new ProcessMovement();
-	
-    public static void main( String[] args )
-    {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	while (sc.hasNext()) {
-            if (sc.hasNextInt())
-                System.out.println(sc.nextInt());
-            else {
-                String inputPattern = sc.next();
-                
-                
-                System.out.println(inputPattern.split(":")[0].split(",")[0]);
-                
-                System.out.println(inputPattern.split(":")[0].split(",")[1]);
-            }
-        }
-    	
-    	sc.close();
-    	
-    	
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {
+			if (sc.hasNextInt())
+				System.out.println(sc.nextInt());
+			else {
+				String inputPattern = sc.next();
+
+
+
+				processMovement.process(
+						Integer.parseInt(inputPattern.split(":")[0].split(",")[0]),
+						Integer.parseInt(inputPattern.split(":")[0].split(",")[1]),
+						inputPattern.split(":")[1]);
+			}
+		}
+
+		sc.close();
+
+	}
 }
--- src/main/java/com/shyju/carParking/ProcessMovement.java
+++ src/main/java/com/shyju/carParking/ProcessMovement.java
@@ -1,11 +1,31 @@
 package com.shyju.carParking;
 
 public class ProcessMovement {
+	
+	String defaultDirection = "N";
+	
+	int index = 0;
+
 
 	public String process(int xPosition, int yPosition, String pattern){
 		
+		System.out.println(pattern);
 		
-		
+		while(pattern.charAt(index)!=-1){
+			
+			switch(pattern.charAt(index)){
+			
+			case 'R': System.out.println("R");
+			break;
+			case 'L':System.out.println("L");
+			break;
+			default: System.out.println("Not both");
+			break;
+			
+			}
+			
+			index++;
+		}
 		
 		return "";
 		
